ec7bec8d9b0b4086a3d59827950aeeaa6ffff3f9
sofiiazarichna <97118195+sofiiazarichna@users.noreply.github.com>
2022-01-28 04:00:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ function storeDisplay() {
         document.getElementById("necklaces").style.display = "block";
     }
 }
+
+var temp = 5;
+
+function accordionDisplay(x) {
+    if (x === temp) {
+        temp = 5;
+        document.getElementById(x).style.display = "none";
+    }
+    else {
+        if (temp !== 5) {
+        document.getElementById(temp).style.display = "none";
+        }
+        temp = x;
+        document.getElementById(x).style.display = "block";
+    }
+}
