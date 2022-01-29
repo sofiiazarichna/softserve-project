adec6fe40710e7b0e085f374de9449f94778b3aa
sofiiazarichna <97118195+sofiiazarichna@users.noreply.github.com>
2022-01-29 21:16:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 function storeDisplay() {
     document.getElementById("earrings").style.display = "none";
     document.getElementById("earringsButtonDiv").classList.remove("checked-radio");
+    document.getElementById("earringsButtonLabel").classList.remove("checked-label");
     document.getElementById("rings").style.display = "none";
     document.getElementById("ringsButtonDiv").classList.remove("checked-radio");
+    document.getElementById("ringsButton").classList.remove("checked-label");
     document.getElementById("necklaces").style.display = "none";
     document.getElementById("necklacesButtonDiv").classList.remove("checked-radio");
+    document.getElementById("necklacesButton").classList.remove("checked-label");
     if (earringsButton.checked == true) {
         document.getElementById("earrings").style.display = "block";
         document.getElementById("earringsButtonDiv").classList.add("checked-radio");
+        document.getElementById("earringsButtonLabel").classList.add("checked-label");
     }
     if (ringsButton.checked == true) {
         document.getElementById("rings").style.display = "block";
         document.getElementById("ringsButtonDiv").classList.add("checked-radio");
+        document.getElementById("ringsButtonLabel").classList.add("checked-label");
     }
     if (necklacesButton.checked == true) {
         document.getElementById("necklaces").style.display = "block";
         document.getElementById("necklacesButtonDiv").classList.add("checked-radio");
+        document.getElementById("necklacesButtonLabel").classList.add("checked-label");
     }
 }
 
