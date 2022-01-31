aa115191049e4f91719dbea2aa3e51f596e8e242
sofiiazarichna <97118195+sofiiazarichna@users.noreply.github.com>
2022-01-31 15:58:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
-function storeDisplay() {
+function storeDisplay(str) {
     document.getElementById("earrings").style.display = "none";
-    document.getElementById("earringsButtonDiv").classList.remove("checked-radio");
-    document.getElementById("earringsButtonLabel").classList.remove("checked-label");
     document.getElementById("rings").style.display = "none";
-    document.getElementById("ringsButtonDiv").classList.remove("checked-radio");
-    document.getElementById("ringsButton").classList.remove("checked-label");
     document.getElementById("necklaces").style.display = "none";
-    document.getElementById("necklacesButtonDiv").classList.remove("checked-radio");
-    document.getElementById("necklacesButton").classList.remove("checked-label");
-    if (earringsButton.checked == true) {
-        document.getElementById("earrings").style.display = "block";
-        document.getElementById("earringsButtonDiv").classList.add("checked-radio");
-        document.getElementById("earringsButtonLabel").classList.add("checked-label");
-    }
-    if (ringsButton.checked == true) {
-        document.getElementById("rings").style.display = "block";
-        document.getElementById("ringsButtonDiv").classList.add("checked-radio");
-        document.getElementById("ringsButtonLabel").classList.add("checked-label");
-    }
-    if (necklacesButton.checked == true) {
-        document.getElementById("necklaces").style.display = "block";
-        document.getElementById("necklacesButtonDiv").classList.add("checked-radio");
-        document.getElementById("necklacesButtonLabel").classList.add("checked-label");
-    }
+    document.getElementById(str).style.display = "block";
+}
+
+function storeActiveButton(str) {
+    document.getElementById("earrings-button").classList.remove("store-button-active");
+    document.getElementById("rings-button").classList.remove("store-button-active");
+    document.getElementById("necklaces-button").classList.remove("store-button-active");
+    document.getElementById(str).classList.add("store-button-active");
 }
 
 var temp = 5;
