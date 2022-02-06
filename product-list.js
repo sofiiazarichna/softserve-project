196dff4e4933490c1eb2d2604b0a08ae9a2d8dd3
sofiiazarichna <97118195+sofiiazarichna@users.noreply.github.com>
2022-02-06 22:59:16 +0200

    Add files via upload

--- product-list.js
+++ product-list.js
@@ -122,7 +122,7 @@
         name: "Butterfly",
         imgUrl: "images/shop-items/butterfly.png",
         imgAlt: "",
-        price: 99.00,
+        price: 99.01,
         description: "This gold pendant in the shape of a butterfly with a natural pearl will remind you of the cheerfulness and playfulness that is inherent in any woman.",
         stock: 10
     },
@@ -159,19 +159,6 @@
         }
     }
 
-/*    function renderProduct(products) {
-        const productsContainer = document.querySelector(".shop-front")
-        for (const product of products) {
-            productsContainer.innerHTML += `
-            <div class="shop-item centered-horizontally">
-                <img src="${product.imgUrl}" alt="${product.imgAlt}" class="shop-item-img responsive" />
-                <h5 class="shop-item-h5">${product.name}</h5>
-                <h3 class="shop-item-h3">$${product.price}</h3>
-                <button class="shop-item-button">— Add to cart</button>
-            </div>`;
-        }
-    }
-*/
     function returnOrder(products, id) {
         for (var i = 0; i < products.length; i++) {
             if (products[i].id === id) {
@@ -183,13 +170,24 @@
 
     function renderProduct(products, id, htmlId) {
         let order = returnOrder(products, id);
+        if (products[order].stock == 0) {
+            document.getElementById(htmlId).innerHTML += `
+            <div class="shop-item centered-horizontally">
+                <img src="${products[order].imgUrl}" alt="${products[order].imgAlt}" style="filter: grayscale(75%);" class="shop-item-img responsive" />
+                <h5 class="shop-item-h5">${products[order].name}</h5>
+                <h3 class="shop-item-h3">$${parseInt(products[order].price).toFixed(2)}</h3>
+                <button class="shop-item-button">Out of stock</button>
+            </div>`;
+        }
+        else {
         document.getElementById(htmlId).innerHTML += `
             <div class="shop-item centered-horizontally">
                 <img src="${products[order].imgUrl}" alt="${products[order].imgAlt}" class="shop-item-img responsive" />
                 <h5 class="shop-item-h5">${products[order].name}</h5>
-                <h3 class="shop-item-h3">$${products[order].price}</h3>
+                <h3 class="shop-item-h3">$${parseInt(products[order].price).toFixed(2)}</h3>
                 <button class="shop-item-button">— Add to cart</button>
             </div>`;
+        }
     }
 
     renderProduct(products, 33, "best-selling-items");
@@ -211,4 +209,12 @@
     renderProduct(products, 31, "necklaces");
     renderProduct(products, 30, "necklaces");
     renderProduct(products, 35, "necklaces");
+
+    function renderStorePage(products) {
+        document.getElementById("store-homepage").innerHTML += `
+            <h4>Store homepage (${products.length})</h4>
+            <h4>Products (${products.length})</h4>
+            <div></div>
+        `;
+    }
 })()   
