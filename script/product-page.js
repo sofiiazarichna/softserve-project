b553e46dfecfbae9995f96cdd39e9148c62c5a94
sofiiazarichna <97118195+sofiiazarichna@users.noreply.github.com>
2022-02-10 23:03:09 +0200

    Update product-page.js

--- script/product-page.js
+++ script/product-page.js
@@ -1,1 +1,12 @@
-//
+function renderPage(products, id) {
+    async function loadData() {
+        const response = await fetch('products.json');
+        const products = await response.json();
+
+    }
+    let order = returnOrder(products, id);
+        if (products[order].stock == 0) {
+            document.innerHTML += `
+            <h2>${products[order].name}</h1>
+            `    
+}
