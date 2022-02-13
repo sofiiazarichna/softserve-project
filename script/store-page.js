723a307b8116ba88eb0ffc076ad8aa9eeb9ede1f
sofiiazarichna <97118195+sofiiazarichna@users.noreply.github.com>
2022-02-13 03:54:54 +0200

    Add files via upload

--- script/store-page.js
+++ script/store-page.js
@@ -1,13 +1,31 @@
-(function() {
+(function () {
     async function loadProducts() {
         const response = await fetch('products.json');
         products = await response.json();
-        renderStorePage();
     }
 
     function renderStorePage(products) {
-        document.main.innerHTML += `
-
-`
+        document.getElementById('main').classList.add("centered-horizontally");
+        document.getElementById('main').innerHTML += `
+            <div class="main-width">
+                <h2 class="homepage">Store homepage (${products.width})</h2>
+                <h2>Products (${products.width})</h2>
+                <div id="store-page-items">
+                </div>
+            </div>
+    `
+        for (product of products) {
+            document.getElementById('store-page-items').innerHTML += `
+                    <div class="store-item">
+                        <img src="${product.imgUrl}" alt="${product.imgAlt}" class="responsive" />
+                        <h5 class="store-item">${product.name}</h5>
+                        <h3 class="store-item">$${parseInt(product.price).toFixed(2)}</h3>
+                        <button class="store-item">Buy</button>
+                    </div>
+    `
+        }
     }
-}) ();
+
+    loadProducts();
+})()
+
