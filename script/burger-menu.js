6648375fc1400c83a5fe1a53ed9c325162a8e77d
sofiiazarichna <97118195+sofiiazarichna@users.noreply.github.com>
2022-02-15 01:49:43 +0200

    Add files via upload

---
+++ script/burger-menu.js
@@ -0,0 +1,5 @@
+function toggleBurger() {
+    document.getElementById("burger-icon-top-bun").classList.toggle("burger-open");
+    document.getElementById("burger-icon-patty").classList.toggle("burger-open");
+    document.getElementById("burger-icon-bottom-bun").classList.toggle("burger-open");
+}
