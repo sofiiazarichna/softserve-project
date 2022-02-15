c19f47fd545578839e931edec04b34fef5036ac1
sofiiazarichna <97118195+sofiiazarichna@users.noreply.github.com>
2022-02-15 14:30:41 +0200

    Add files via upload

--- script/product-info.js
+++ script/product-info.js
@@ -3,11 +3,12 @@
     function showInfo() {
         const product = JSON.parse(localStorage.product || "{}");
         if (!product) return;
-        document.body.innerHTML += `${product.name}`;
+        
     }
 
     showInfo();
     function renderPage() {
+    document.body.innerHTML += `${product.name}`;
     document.body.innerHTML += `
     <main id="main" class="centered-horizontally">
         <div class="main-width">
@@ -16,12 +17,12 @@
                 <div class="left centered-horizontally"><img src="images/shop-items/athena.png" class="product-info" /></div>
                 <div class="right">
                     <p class="stocked">In stock</p>
-                    <h4 class="product-info">Athena</h4>
-                    <h3 class="product-info">Product code 30</h3>
-                    <h5 class="product-info">$199,00</h5>
+                    <h4 class="product-info">${product.name}</h4>
+                    <h3 class="product-info">Product code ${product.id}</h3>
+                    <h5 class="product-info">$${parseInt(products[order].price).toFixed(2)}</h5>
                     <button class="product-info">Add to cart</button>
                     <div class="product-info"><h6 class="product-info">DESCRIPTION</h6></div>
-                    <p class="product-info">The necklace in the ancient Greek style beckons with its elegance. It is perfect for everyday looks and evening dresses. The patterns are made of gold by hand.</p>
+                    <p class="product-info">${product.description}</p>
                 </div>
             </div>
         </div>
